7358d4b0f7ed293cf861037d9038dbff345456b6
elama <akalend@mail.ru>
2015-08-12 18:19:14 +0300

    add interface AMQPConnection

--- ext_amqp.php
+++ ext_amqp.php
@@ -1,6 +1,30 @@
 <?hh
-class Connection {
-  
-  public function __construct(protected string $name = 'Stranger') {}
 
+class AMQPConnection {
+	/* Methods */
+	
+	public  function __construct (){ }
+
+	public function  connect ( array $parms = []){}
+	public function  disconnect ( ){}
+	public function  getHost ( ){}
+	public function  getLogin ( ){}
+	public function  getPassword ( ){}
+	public function  getPort ( ){}
+	public function  getVhost ( ){}
+	public function  isConnected ( ){}
+	public function  reconnect ( ){}
+	public function  setHost ( string $host ){}
+	public function  setLogin ( string $login ){}
+	public function  setPassword ( string $password ){}
+	public function  setPort ( int $port ){}
+	public function  setVhost (  $vhost ){}
+
+	/* internal */
+	private $host = 'localhost';
+	private $port = '5672';
+	private $Login = 'quest';
+	private $password = '';
+	private $vhost = '/';
+	private $parms = [];
 }
--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
-	print_r(get_loaded_extensions());
-//	$cnn = new Connection();
+	// print_r( get_loaded_extensions() );
+	$cnn = new AMQPConnection();
