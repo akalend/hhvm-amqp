3ad906e62d67bdcc9445a387b25761f3558329f0
elama <akalend@mail.ru>
2015-08-12 18:40:09 +0300

    fix interface

--- ext_amqp.php
+++ ext_amqp.php
@@ -1,30 +1,78 @@
 <?hh
 
 class AMQPConnection {
-	/* Methods */
 	
-	public  function __construct (){ }
-
-	public function  connect ( array $parms = []){}
-	public function  disconnect ( ){}
-	public function  getHost ( ){}
-	public function  getLogin ( ){}
-	public function  getPassword ( ){}
-	public function  getPort ( ){}
-	public function  getVhost ( ){}
-	public function  isConnected ( ){}
-	public function  reconnect ( ){}
-	public function  setHost ( string $host ){}
-	public function  setLogin ( string $login ){}
-	public function  setPassword ( string $password ){}
-	public function  setPort ( int $port ){}
-	public function  setVhost (  $vhost ){}
-
 	/* internal */
 	private $host = 'localhost';
-	private $port = '5672';
-	private $Login = 'quest';
-	private $password = '';
+	private $port = 5672;
+	private $login = 'quest';
 	private $vhost = '/';
-	private $parms = [];
+	private $password = '';
+
+
+	/* Methods */
+	public  function __construct (array $parms = []){ 
+		if (isset($parms['host']))
+			$this->host = $parms['host'];
+		
+		if (isset($parms['port']))
+			$this->port = $parms['port'];
+		
+		if (isset($parms['login']))
+			$this->login = $parms['login'];
+		
+		if (isset($parms['vhost']))
+			$this->vhost = $parms['vhost'];
+		
+		if (isset($parms['password']))
+			$this->password = $parms['password'];
+	}
+
+	public function  setHost ( string $host ){
+		$this->host = $host;
+	}
+
+	public function  setLogin ( string $login ){
+		$this->login = $login;
+	}
+
+	public function  setPassword ( string $password ){
+		$this->password = $password;
+	}
+
+	public function  setPort ( int $port ){
+		$this->port = $port;
+	}
+
+	public function  setVhost ( string $vhost ){
+		$this->vhost = $vhost;
+	}
+
+	public function  connect ( ){}
+
+	public function  disconnect ( ){}
+	
+	public function  getHost (){
+		return $this->host;
+	}
+
+	public function  getLogin (){
+		return $this->login;
+	}
+
+	public function  getPassword (){
+		return $this->password;
+	}
+
+	public function  getPort ( ){
+		return $this->port;
+	}
+
+	public function  getVhost (){
+		return $this->vhost;
+	}
+
+	public function  isConnected ( ){}
+	public function  reconnect ( ){}
+	
 }
--- test.php
+++ test.php
@@ -1,3 +1,5 @@
 <?php
 	// print_r( get_loaded_extensions() );
-	$cnn = new AMQPConnection();
+	$cnn = new AMQPConnection(['host'=>'ak']);
+	print_r($cnn->getHost());
+	print_r($cnn->getPort());
