9d87cef15fdfd5d8506e30c6ad2e2f95494ef44c
elama <akalend@mail.ru>
2015-08-13 17:49:47 +0300

    add AMQPQueue interface

--- ext_amqp.php
+++ ext_amqp.php
@@ -98,10 +98,40 @@ class AMQPConnection {
 	/* Methods */	
 	function __construct ( $amqp_connection ) {}
 	public function commitTransaction () {}
-	public function isConnected ( void ): void
+	public function isConnected ( void ){}
 	public function qos ( int $size , int $count ) {}
 	public function rollbackTransaction ( void ) {}
 	public function setPrefetchCount ( int $count ) {}
 	public function setPrefetchSize ( int $size ) {}
 	public function startTransaction ( void ) {}
+}
+
+const int AMQP_NOPARAM = 0;
+const int AMQP_NOACK = 1;
+
+ AMQPQueue {
+	/* Methods */
+	public function ack ( int $delivery_tag , int $flags = AMQP_NOPARAM){}
+	public function bind ( string $exchange_name , string $routing_key ){}
+	public function cancel ([ string $consumer_tag = "" ] ){}
+	public function __construct ( $amqp_channel ){
+		// return : AMQPChannel	
+	}
+	public function consume ( callable $callback [, int $flags = AMQP_NOPARAM ] ){}
+	public function declare ( void ): int
+	publicfunction delete ( void ){}
+	public function get ([ int $flags= AMQP_NOACK]){
+		//: mixed
+	}
+	public function getArgument ( string $key ): mixed
+	public function getArguments ( void ): array
+	public function getFlags ( void ): int
+	public function getName ( void ): string
+	public function nack ( string $delivery_tag, string $flags = AMQP_NOPARAM  ){}
+	public function purge ( void ){}
+	public function setArgument ( string $key , mixed $value ){}
+	public function setArguments ( array $arguments ){}
+	public function setFlags ( int $flags ){}
+	public function setName ( string $queue_name ){}
+	public function unbind ( string $exchange_name , string $routing_key ){}
 }
