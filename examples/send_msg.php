8b8dc628d7ca2f7a53d52c34abf497bb7630381b
akalend <akalend@mail.ru>
2015-09-08 23:51:17 +0300

    AMQPExchange::publish()

--- examples/send_msg.php
+++ examples/send_msg.php
@@ -1,1 +1,20 @@
 <?php
+// print_r( get_loaded_extensions() );
+	$cnn = new AMQPConnection();
+	echo "------  connect ...  --------\n";
+
+	$ret = $cnn->connect();
+
+	echo "------  connect Ok  --------\n";
+
+	$ch = new AMQPChannel($cnn);
+	$ex = new AMQPExchange($ch);
+
+	echo "------  exchange Ok  --------\n";
+
+// $cnn->disconnect(AMQP_NOPARAM);
+	$ex->setName('test_e');
+	
+	$ex->publish('*******', 'scan');
+
+	$cnn->disconnect();
